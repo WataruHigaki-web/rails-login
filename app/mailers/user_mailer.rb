0b9a1b902dcf53340998c8c6bb0cfa3de178a89a
EC2 Default User <ec2-user@ip-172-31-35-228.ap-northeast-1.compute.internal>
2020-02-10 04:09:06 +0000

    Add acount activation

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,26 @@
+class UserMailer < ApplicationMailer
+
+  def account_activation(user)
+    @user = user
+    mail to:user.email,subject: "Account activation"
+  end
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.user_mailer.password_reset.subject
+  #
+  
+  # トークンがダイジェストと一致したらtrueを返す
+  def authenticated?(attribute, token)
+    digest = send("#{attribute}_digest")
+    return false if digest.nil?
+    BCrypt::Password.new(digest).is_password?(token)
+  end
+  
+  def password_reset
+    @greeting = "Hi"
+
+    mail to: "to@example.org"
+  end
+end
--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -1,6 +1,13 @@
 require 'test_helper'
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
+    
+  def setup
+    ActionMailer::Base.deliveries.clear
+  end
+    
+    
+    
     test "invalid signup information" do
     get signup_path
     assert_no_difference 'User.count' do
@@ -14,7 +21,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert_select 'div.<CSS class for field with error>'
   end
   
-    test "valid signup information" do
+   test "valid signup information with account activation" do
     get signup_path
     assert_difference 'User.count', 1 do
       post users_path, params: { user: { name:  "Example User",
